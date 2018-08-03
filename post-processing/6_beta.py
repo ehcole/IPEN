2a9e47b53c34524e6f16d8aeb90e440baa5c3786
ehcole <ehcole@umich.edu>
2018-08-03 15:41:47 -0400

    Update 6_beta.py

--- post-processing/6_beta.py
+++ post-processing/6_beta.py
@@ -3,8 +3,9 @@ import numpy as np
 import sys
 import os
 
-#TODO: Replace with to h5 file
-f = h5py.File("/scratch/wrm_fluxoe/ehcole/IPEN_Analysis/version2/IPEN-008/6.h5", 'r')
+#TODO: define path to top level git dir
+pathToTopLevel = '.'
+f = h5py.File(pathToTopLevel + "/IPEN/experiments/6/outputs/3D/6.h5", 'r')
 beta = f["STATE_0002/beta"][()]
 genTime = f["STATE_0002/generation_time"][()] * 10**6
 print("Beta Effective:", beta * 10**5)
