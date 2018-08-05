b261a6f4b482cb4316925f0afbb2bb4f769e3bb1
ehcole <ehcole@umich.edu>
2018-08-05 15:23:39 -0400

    Update pinPowers.sh

--- experiments/6/sensitivities/pinPowers.sh
+++ experiments/6/sensitivities/pinPowers.sh
@@ -1,16 +1,37 @@
 #!/bin/bash
+echo "case 1: control rods in"
+echo
 echo "low buckling"
-python /scratch/wrm_fluxoe/ehcole/IPEN_Analysis/analysis/pinPowers.py /scratch/wrm_fluxoe/ehcole/IPEN_Analysis/version2/IPEN-009bm/IPEN-009-1p.h5
+python ../../../post-processing/pinPowers.py ../outputs/2D/in/6-1_f.h5
 echo "med buckling"
-python /scratch/wrm_fluxoe/ehcole/IPEN_Analysis/analysis/pinPowers.py /scratch/wrm_fluxoe/ehcole/IPEN_Analysis/version2/IPEN-009bm/IPEN-009-2p.h5
+python ../../../post-processing/pinPowers.py ../outputs/2D/in/6-2_f.h5
 echo "high buckling"
-python /scratch/wrm_fluxoe/ehcole/IPEN_Analysis/analysis/pinPowers.py /scratch/wrm_fluxoe/ehcole/IPEN_Analysis/version2/IPEN-009bm/IPEN-009-3p.h5
+python ../../../post-processing/pinPowers.py ../outputs/2D/in/6-3_f.h5
 echo
 echo "reference:"
 echo
 echo "low buckling"
-python /scratch/wrm_fluxoe/ehcole/IPEN_Analysis/analysis/pinPowers.py /scratch/wrm_fluxoe/ehcole/IPEN_Analysis/version2/IPEN-009b/IPEN-009-1.h5
+python ../../../post-processing/pinPowers.py ../outputs/2D/in/6-1.h5
 echo "med buckling"
-python /scratch/wrm_fluxoe/ehcole/IPEN_Analysis/analysis/pinPowers.py /scratch/wrm_fluxoe/ehcole/IPEN_Analysis/version2/IPEN-009b/IPEN-009-2.h5
+python ../../../post-processing/pinPowers.py ../outputs/2D/in/6-2.h5
 echo "high buckling"
-python /scratch/wrm_fluxoe/ehcole/IPEN_Analysis/analysis/pinPowers.py /scratch/wrm_fluxoe/ehcole/IPEN_Analysis/version2/IPEN-009b/IPEN-009-3.h5
+python ../../../post-processing/pinPowers.py ../outputs/2D/in/6-3.h5
+echo
+echo "case 2: control rods out"
+echo
+
+echo "low buckling"
+python ../../../post-processing/pinPowers.py ../outputs/2D/out/6-1_f.h5
+echo "med buckling"
+python ../../../post-processing/pinPowers.py ../outputs/2D/out/6-2_f.h5
+echo "high buckling"
+python ../../../post-processing/pinPowers.py ../outputs/2D/out/6-3_f.h5
+echo
+echo "reference:"
+echo
+echo "low buckling"
+python ../../../post-processing/pinPowers.py ../outputs/2D/out/6-1.h5
+echo "med buckling"
+python ../../../post-processing/pinPowers.py ../outputs/2D/out/6-2.h5
+echo "high buckling"
+python ../../../post-processing/pinPowers.py ../outputs/2D/out/6-3.h5
