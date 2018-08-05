7a90b7279e21f41d0919613221919ebbfa7b9155
ehcole <ehcole@umich.edu>
2018-08-05 12:44:03 -0400

    fixed typo in configure

--- post-processing/configure.py
+++ post-processing/configure.py
@@ -1,15 +1,32 @@
 import os
+
 import fileinput
+
 pwd = os.getcwd()
+
 path = str()
+
 for char in pwd:
+
   if char != '\\':
+
     path += char
+
   else: 
+
     path += '/'
-os.system("mv " + path + "/experimental_data " + path[:-16]) 
+
+os.system("mv " + path + "/experimental_data " + path[:-16])
+
 # Does a list of files, and
+
 # redirects STDOUT to the file in question
+
 for line in fileinput.input(os.listdir(path), inplace = 1):
-    print(line.replace("\'.\'", path[:-16]),
+
+    print(line.replace("\'.\'", path[:-16]))
+
+
+
 os.system("mv " + path[:-16] + "/experimental_data " + path) 
+
