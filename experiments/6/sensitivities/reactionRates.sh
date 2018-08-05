19ccd4802b08c96426b6ccaa1c82873d333dbe00
ehcole <ehcole@umich.edu>
2018-08-05 15:25:21 -0400

    Update reactionRates.sh

--- experiments/6/sensitivities/reactionRates.sh
+++ experiments/6/sensitivities/reactionRates.sh
@@ -1,16 +1,37 @@
 #!/bin/bash
+echo "case 1: control rods in"
+echo
 echo "low buckling"
-python /scratch/wrm_fluxoe/ehcole/IPEN_Analysis/analysis/reactionRates.py /scratch/wrm_fluxoe/ehcole/IPEN_Analysis/version2/IPEN-009bm/IPEN-009-1.h5
+python ../../../post-processing/reactionRates.py ../outputs/2D/in/6-1_f_r.h5
 echo "med buckling"
-python /scratch/wrm_fluxoe/ehcole/IPEN_Analysis/analysis/reactionRates.py /scratch/wrm_fluxoe/ehcole/IPEN_Analysis/version2/IPEN-009bm/IPEN-009-2.h5
+python ../../../post-processing/reactionRates.py ../outputs/2D/in/6-2_f_r.h5
 echo "high buckling"
-python /scratch/wrm_fluxoe/ehcole/IPEN_Analysis/analysis/reactionRates.py /scratch/wrm_fluxoe/ehcole/IPEN_Analysis/version2/IPEN-009bm/IPEN-009-3.h5
+python ../../../post-processing/reactionRates.py ../outputs/2D/in/6-3_f_r.h5
 echo
 echo "reference:"
 echo
 echo "low buckling"
-python /scratch/wrm_fluxoe/ehcole/IPEN_Analysis/analysis/reactionRates.py /scratch/wrm_fluxoe/ehcole/IPEN_Analysis/version2/IPEN-009b/IPEN-009-1.h5
+python ../../../post-processing/reactionRates.py ../outputs/2D/in/6-1_r.h5
 echo "med buckling"
-python /scratch/wrm_fluxoe/ehcole/IPEN_Analysis/analysis/reactionRates.py /scratch/wrm_fluxoe/ehcole/IPEN_Analysis/version2/IPEN-009b/IPEN-009-2.h5
+python ../../../post-processing/reactionRates.py ../outputs/2D/in/6-2_r.h5
 echo "high buckling"
-python /scratch/wrm_fluxoe/ehcole/IPEN_Analysis/analysis/reactionRates.py /scratch/wrm_fluxoe/ehcole/IPEN_Analysis/version2/IPEN-009b/IPEN-009-3.h5
+python ../../../post-processing/reactionRates.py ../outputs/2D/in/6-3_r.h5
+echo
+echo "case 2: control rods out"
+echo
+
+echo "low buckling"
+python ../../../post-processing/reactionRates.py ../outputs/2D/out/6-1_f_r.h5
+echo "med buckling"
+python ../../../post-processing/reactionRates.py ../outputs/2D/out/6-2_f_r.h5
+echo "high buckling"
+python ../../../post-processing/reactionRates.py ../outputs/2D/out/6-3_f_r.h5
+echo
+echo "reference:"
+echo
+echo "low buckling"
+python ../../../post-processing/reactionRates.py ../outputs/2D/out/6-1_r.h5
+echo "med buckling"
+python ../../../post-processing/reactionRates.py ../outputs/2D/out/6-2_r.h5
+echo "high buckling"
+python ../../../post-processing/reactionRates.py ../outputs/2D/out/6-3_r.h5
