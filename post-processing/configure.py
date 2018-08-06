3fbaafbb7f10fdb56013f25368b360a0f21afa38
ehcole <ehcole@umich.edu>
2018-08-06 11:38:09 -0400

    updated configure to make repo reconfigurable

--- post-processing/configure.py
+++ post-processing/configure.py
@@ -1,34 +1,136 @@
 import os
 
+
+
+
+
+
+
 import fileinput
+
+
+
+
+
+
 
 pwd = os.getcwd()
 
+
+
+
+
+
+
 path = str()
+
+
+
+
+
+
 
 for char in pwd:
 
+
+
+
+
+
+
   if char != '\\':
+
+
+
+
+
+
 
     path += char
 
+
+
+
+
+
+
   else: 
+
+
+
+
+
+
 
     path += '/'
 
+
+
+
+
+
+
 os.system("mv " + path + "/experimental_data " + path[:-16])
+
+
+
+
+
+
 
 # Does a list of files, and
 
+
+
+
+
+
+
 # redirects STDOUT to the file in question
 
+
+
+
+
+
+
 for line in fileinput.input(os.listdir(path), inplace = 1):
-  print(line.replace("\'.\'", "'" + path[:-16] + "'"))
-  #if "pathToTopLevel =" in line:
-    #print(line.split("=")[0] + "= '" + path[:-16] + "'")
-  #else:
-   # print(line)
+
+
+
+#  print(line.replace("\'.\'", "'" + path[:-16] + "'"))
+
+
+
+  if "pathToTopLevel =" in line and not ":" in line:
+
+
+
+    print(line.split("=")[0] + "= '" + path[:-16] + "'")
+
+
+
+  else:
+
+
+
+    print(line)
+
+
+
+
+
+
+
+
+
 
 
 os.system("mv " + path[:-16] + "/experimental_data " + path) 
+
+
+
+
+
+
 
